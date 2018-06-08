ac24a2c7b6800e19dfedf078bafd04418daaf6a9
david.dimon <david_dimon@hotmail.com>
2018-06-08 20:02:18 -0300

    First commit

---
+++ src/main/java/com/achadoperdido/model/Achado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.achadoperdido.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author david
+ */
+public class Achado {
+    
+    private Integer id;
+    private String cor;
+    private String modelo;
+    private Date dataAchado;
+    private String local;
+    private String telefone;
+    private String email;
+    private String nomeEncontrou;
+}
