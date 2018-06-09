2bf12b86e34e5aa8b9d867687edb7ef32b0de15f
Bruno Bernardo <brunobernardo@progat.com.br>
2018-06-08 22:38:35 -0300

    Criado classe perdido, e getters e setters das classes

--- src/main/java/com/achadoperdido/model/Achado.java
+++ src/main/java/com/achadoperdido/model/Achado.java
@@ -21,4 +21,82 @@ public class Achado {
     private String telefone;
     private String email;
     private String nomeEncontrou;
+    
+	public Achado(Integer id, String cor, String modelo, Date dataAchado, String local, String telefone, String email,
+			String nomeEncontrou) {
+		super();
+		this.id = id;
+		this.cor = cor;
+		this.modelo = modelo;
+		this.dataAchado = dataAchado;
+		this.local = local;
+		this.telefone = telefone;
+		this.email = email;
+		this.nomeEncontrou = nomeEncontrou;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public Date getDataAchado() {
+		return dataAchado;
+	}
+	
+	public void setDataAchado(Date dataAchado) {
+		this.dataAchado = dataAchado;
+	}
+	
+	public String getLocal() {
+		return local;
+	}
+	
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNomeEncontrou() {
+		return nomeEncontrou;
+	}
+	
+	public void setNomeEncontrou(String nomeEncontrou) {
+		this.nomeEncontrou = nomeEncontrou;
+	}
+    
 }
